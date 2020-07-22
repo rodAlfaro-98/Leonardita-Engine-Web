ba6882222c2f60b106e758f51a8522015fbe151d
Gerardo <gerhdev2712@gmail.com>
2020-07-22 17:47:24 -0500

    Enlace de Bootstrap completo. Creada pagina de inicio, sesion y registro. Problemas de Scroll generalizados

--- Leonardita-EGE-Web/App/resources/views/layouts/app.blade.php
+++ Leonardita-EGE-Web/App/resources/views/layouts/app.blade.php
@@ -19,62 +19,70 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body class="container-fluid overflow-hidden ">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
 
-                    </ul>
+    <nav class="row justify-content-between navbar p-3 m-2 navbar-expand-lg navbar-light bg-light">
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+        <div class="col">
+            <a class="navbar-brand" href="{{ url('/') }}">Inicio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+        <div class="col">
+            <div class="justify-content-end collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                    </li>
+
+                </ul>
             </div>
-        </nav>
+        </div>
 
-        <main class="py-4">
-            @yield('content')
+
+    </nav>
+    
+
+    <div class="row mt-5 pt-3 justify-content-around">
+
+        <main class="col-sm-12 col-md-7 m-2 p-5">
+            <div class="row">
+                @yield('content')
+            </div>
         </main>
+
+
+        <aside class="col-sm-12 col-md-4 m-2 p-5">
+            <div class="row justify-content-around">
+
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+
+                @yield('aside_content')
+            </div>
+        </aside>
+
     </div>
+
+
+    <footer class="row">
+        
+    </footer>
+
+    
 </body>
 </html>
