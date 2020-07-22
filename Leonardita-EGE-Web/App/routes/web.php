6cffbf4c4832b99134b38425a3bb1ca51f10b9ea
Gerardo <gerhdev2712@gmail.com>
2020-07-22 16:02:20 -0500

    Solucion bootstrap

--- Leonardita-EGE-Web/App/routes/web.php
+++ Leonardita-EGE-Web/App/routes/web.php
@@ -24,7 +24,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
